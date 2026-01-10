e8019e618400a917d39163dc36bbd0a068b7fc50
Vipin Karade <karadevipin@gmail.com>
2026-01-10 13:29:55 +0530

    Valid Parenthesis

---
+++ src/stack/ValidParentheses.java
@@ -0,0 +1,57 @@
+package stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
+public class ValidParentheses
+{
+	static void main()
+	{
+//		String s = "{[]}";
+		String s = "{[()]]}";
+		System.out.println(isValid(s));
+	}
+
+	private static boolean isValid(String s)
+	{
+		Map<Character, Character> refMap = new HashMap<>();
+		refMap.put(')', '(');
+		refMap.put('}', '{');
+		refMap.put(']', '[');
+
+		Set<Character> closingVal = refMap.keySet();
+
+		Stack<Character> stack = new Stack<>();
+
+		for (int i = 0; i < s.length(); i++)
+		{
+			Character curr = s.charAt(i);
+
+			if (stack.size() == 0)
+			{
+				if (closingVal.contains(curr))
+					return false;
+				else
+					stack.addLast(curr);
+			}
+			else
+			{
+				if (closingVal.contains(curr))
+				{
+					if (stack.getLast() == refMap.get(curr))
+						stack.removeLast();
+					else
+						return false;
+				}
+				else
+					stack.addLast(curr);
+			}
+
+		}
+		return stack.isEmpty();
+	}
+}

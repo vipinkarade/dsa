1b3cbf98d7406a7d04c563d1baa35d5137a6ec59
Vipin Karade <karadevipin@gmail.com>
2026-03-20 21:32:43 +0530

    Coin Change II

---
+++ src/twoDDP/CoinChangeII.java
@@ -0,0 +1,31 @@
+package twoDDP;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CoinChangeII
+{
+	static void main()
+	{
+
+	}
+
+	private static int change(int amount, int[] coins)
+	{
+		int[] result = new int[amount + 1];
+		result[0] = 1;
+
+		for(int coin : coins)
+		{
+			for (int i = 1; i < result.length; i++)
+			{
+				if (i - coin >= 0 && result[i - coin] > 0)
+					result[i]++;
+			}
+		}
+		int ans = 0;
+		for (int res : result)
+			ans += res;
+		return ans;
+	}
+}
